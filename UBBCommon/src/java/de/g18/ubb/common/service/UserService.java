7c140fdb5c9d0d53a8d7fdca8f37c4279b0b2300
Kevin Huber <kevinhuber.kh@gmail.com>
2012-08-16 16:34:30 +0200

    Annotations korrigiert

--- UBBCommon/src/java/de/g18/ubb/common/service/UserService.java
+++ UBBCommon/src/java/de/g18/ubb/common/service/UserService.java
@@ -2,6 +2,7 @@ package de.g18.ubb.common.service;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -11,12 +12,13 @@ import de.g18.ubb.common.domain.User;
 /**
  * @author <a href="mailto:kevinhuber.kh@gmail.com">Kevin Huber</a>
  */
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public interface UserService extends StoreService<User>,
                                      LoadService<User> {
 
     public static final String RESTFUL_SERVICE_NAME = "UserService";
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
     List<User> getAll();
 }
