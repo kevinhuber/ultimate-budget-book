b8cf5638ba08f8e6f8c103af029355e1d9a11a16
Kevin Huber <kevinhuber.kh@gmail.com>
2012-08-17 11:26:51 +0200

    Post-Parameter korrigiert

--- UBBCommon/src/java/de/g18/ubb/common/service/UserService.java
+++ UBBCommon/src/java/de/g18/ubb/common/service/UserService.java
@@ -4,13 +4,13 @@ import java.util.List;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import de.g18.ubb.common.domain.User;
-import de.g18.ubb.common.service.exception.NotFoundExcpetion;
 
 /**
  * @author <a href="mailto:kevinhuber.kh@gmail.com">Kevin Huber</a>
@@ -28,8 +28,6 @@ public interface UserService {
     User saveAndLoad(User aEntity);
 
     @POST
-    User loadById(Long aId) throws NotFoundExcpetion;
-    
-    @POST
-    boolean login(String aEmail, String aPassword);
+    boolean login(@HeaderParam("email") String aEmail,
+                  @HeaderParam("password") String aPassword);
 }
