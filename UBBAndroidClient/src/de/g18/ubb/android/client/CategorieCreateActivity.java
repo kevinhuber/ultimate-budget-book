574d70ce8ee34ab761680fd9ae8cce1b5eac31e2
Daniel <daniel.benjamin.fels@gmail.com>
2012-08-20 18:42:28 +0200

    categorie create activity

---
+++ UBBAndroidClient/src/de/g18/ubb/android/client/CategorieCreateActivity.java
@@ -0,0 +1,35 @@
+package de.g18.ubb.android.client;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+public class CategorieCreateActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categorie_create);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_categorie_create, menu);
+        return true;
+    }
+
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
