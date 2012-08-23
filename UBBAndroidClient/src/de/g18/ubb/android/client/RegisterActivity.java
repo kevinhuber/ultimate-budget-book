7f74d6d8de8582c67104a5f55b289db440a3efcf
Daniel <daniel.benjamin.fels@gmail.com>
2012-08-23 12:56:44 +0200

    Register Activity

--- UBBAndroidClient/src/de/g18/ubb/android/client/RegisterActivity.java
+++ UBBAndroidClient/src/de/g18/ubb/android/client/RegisterActivity.java
@@ -1,10 +1,20 @@
 package de.g18.ubb.android.client;
 
+import de.g18.ubb.android.client.communication.WebServiceProvider;
+import de.g18.ubb.common.service.repository.ServiceRepository;
+import de.g18.ubb.common.util.ObjectUtil;
+import de.g18.ubb.common.util.StringUtil;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends Activity {
 
@@ -13,6 +23,9 @@ public class RegisterActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 //        getActionBar().setDisplayHomeAsUpEnabled(true);
+        
+        Button register = (Button) findViewById(R.id.b_register);
+        register.setOnClickListener(new RegisterButtonListener());
     }
 
     @Override
@@ -30,5 +43,57 @@ public class RegisterActivity extends Activity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+    
+    // -------------------------------------------------------------------------
+    // Inner Classes
+    // -------------------------------------------------------------------------
+
+    private final class RegisterButtonListener implements OnClickListener {
+
+        public void onClick(View aView) {
+        	EditText e_anzeigename = (EditText) findViewById(R.id.e_anzeigename);
+        	String aUsername = e_anzeigename.getText().toString();
+        	
+        	EditText e_email = (EditText) findViewById(R.id.e_email);
+        	String aEMail = e_email.getText().toString();
+        	
+        	EditText e_password1 = (EditText) findViewById(R.id.e_password1);
+        	String password1 = e_password1.getText().toString();
+        	
+        	EditText e_password2 = (EditText) findViewById(R.id.e_password2);
+        	String password2 = e_password2.getText().toString();
+        	
+        	
+        	
+        	if (checkNotEmpty(aUsername) && (checkNotEmpty(aEMail))) {
+        		if (!ServiceRepository.getUserService().existsUserWithEMail(aEMail)) {
+        			if (checkPw(password1, password2)) {
+        				ServiceRepository.getUserService().register(aEMail, aUsername, password1);
+        				Toast.makeText(getApplicationContext(), "Registration erfolgreich!!!", Toast.LENGTH_SHORT).show();
+        				WebServiceProvider.authentificate(aUsername, password1);
+        				Intent i = new Intent(getApplicationContext(), BudgetBookOverviewActivity.class);
+        				startActivity(i);
+        				return;
+        				
+        			} else {
+    					Toast.makeText(getApplicationContext(), "Passwörter stimmen nicht überein", Toast.LENGTH_LONG).show();
+    					return;
+        			}
+				} else {
+					Toast.makeText(getApplicationContext(), "EMail wird bereits verwendet", Toast.LENGTH_LONG).show();
+					return;
+				}
+        		
+        	}
+        }
+        
+        private boolean checkPw(String pw1, String pw2){
+        	return ObjectUtil.equals(pw1, pw2);
+        }
+        
+        private boolean checkNotEmpty(String input){
+        	return !StringUtil.isEmpty(input);
+        }
     }
 }
