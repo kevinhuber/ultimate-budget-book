0850675230ae524ee4786bf2b734bea19c07e351
Kevin Huber <kevinhuber.kh@gmail.com>
2012-08-28 16:57:47 +0200

    Oberklasse für Entitäten erstellt, die nicht persistiert werden müssen

--- UBBCommon/src/java/de/g18/ubb/common/domain/AbstractEntity.java
+++ UBBCommon/src/java/de/g18/ubb/common/domain/AbstractEntity.java
@@ -1,31 +1,22 @@
 package de.g18.ubb.common.domain;
 
-import java.beans.PropertyChangeListener;
-import java.beans.PropertyChangeSupport;
-import java.io.Serializable;
-
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
-import javax.persistence.Transient;
 
 /**
  * @author <a href="mailto:kevinhuber.kh@gmail.com">Kevin Huber</a>
  */
 @MappedSuperclass
-public abstract class AbstractEntity implements Identifiable, Serializable {
+public abstract class AbstractEntity extends AbstractModel implements Identifiable {
 
     private static final long serialVersionUID = 1L;
-
-    @Transient
-    private final PropertyChangeSupport changeSupport;
 
     private Long id;
 
 
-    protected AbstractEntity() {
-        changeSupport = new PropertyChangeSupport(this);
+    public AbstractEntity() {
     }
 
     @Override
@@ -68,20 +59,5 @@ public abstract class AbstractEntity implements Identifiable, Serializable {
 
         Identifiable otherEntity = (Identifiable) aObject;
         return getId() == otherEntity.getId();
-    }
-
-    protected final void fireChange(String aPropertyName, Object aOldValue, Object aNewValue) {
-        if (aOldValue == aNewValue) {
-            return;
-        }
-        changeSupport.firePropertyChange(aPropertyName, aOldValue, aNewValue);
-    }
-
-    public final void removePropertyChangeListener(String aPropertyName, PropertyChangeListener aListener) {
-        changeSupport.removePropertyChangeListener(aPropertyName, aListener);
-    }
-
-    public final void addPropertyChangeListener(String aPropertyName, PropertyChangeListener aListener) {
-        changeSupport.addPropertyChangeListener(aPropertyName, aListener);
     }
 }
