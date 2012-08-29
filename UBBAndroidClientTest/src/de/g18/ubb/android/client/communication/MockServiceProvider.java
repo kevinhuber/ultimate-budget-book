3948ba2a25762f5fa5d03af8e7bc1cb181939f38
Kevin Huber <kevinhuber.kh@gmail.com>
2012-08-29 16:50:39 +0200

    Eigene Mock-Service-Implementierung erlauben

--- UBBAndroidClientTest/src/de/g18/ubb/android/client/communication/MockServiceProvider.java
+++ UBBAndroidClientTest/src/de/g18/ubb/android/client/communication/MockServiceProvider.java
@@ -15,6 +15,10 @@ import de.g18.ubb.common.service.repository.ServiceRepository;
  */
 public final class MockServiceProvider implements ServiceProvider {
 
+    private static final String MOCK_SERVICE_BASE_PACKAGE = "de.g18.ubb.android.client.mock.service.";
+    private static final String MOCK_SERVICE_PREFIX = "Mock";
+    private static final String MOCK_SERVICE_SUFFIX = "Impl";
+
     private static MockServiceProvider instance;
     private static boolean registered;
 
@@ -45,8 +49,32 @@ public final class MockServiceProvider implements ServiceProvider {
     }
 
     @Override
-    @SuppressWarnings("unchecked")
     public <_Service> _Service lookup(Class<_Service> aServiceClass) {
+        Class<_Service> mockedServiceClass = getMockedServiceClass(aServiceClass);
+        if (mockedServiceClass == null) {
+            return createMockedProxy(aServiceClass);
+        }
+        try {
+            return mockedServiceClass.newInstance();
+        } catch (Exception e) {
+            throw new IllegalStateException("Can not create new mocked instance for " + aServiceClass.getName() + "!", e);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private <_Service> Class<_Service> getMockedServiceClass(Class<_Service> aServiceClass) {
+        try {
+            return (Class<_Service>) Class.forName(MOCK_SERVICE_BASE_PACKAGE
+                                                 + MOCK_SERVICE_PREFIX
+                                                 + aServiceClass.getSimpleName()
+                                                 + MOCK_SERVICE_SUFFIX);
+        } catch (ClassNotFoundException e) {
+        }
+        return null;
+    }
+
+    @SuppressWarnings("unchecked")
+    private <_Service> _Service createMockedProxy(Class<_Service> aServiceClass) {
         return (_Service) Proxy.newProxyInstance(aServiceClass.getClassLoader(),
                                                  new Class[] {aServiceClass},
                                                  new MockService());
