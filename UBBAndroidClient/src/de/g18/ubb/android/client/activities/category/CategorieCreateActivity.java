9e4a9ba4265d3903cd481a5c2a2ade828fe5a144
Daniel <daniel.benjamin.fels@gmail.com>
2012-08-30 12:13:01 +0200

    Categorie Create actionlistener impl

--- UBBAndroidClient/src/de/g18/ubb/android/client/activities/category/CategorieCreateActivity.java
+++ UBBAndroidClient/src/de/g18/ubb/android/client/activities/category/CategorieCreateActivity.java
@@ -1,18 +1,32 @@
 package de.g18.ubb.android.client.activities.category;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 import de.g18.ubb.android.client.R;
+import de.g18.ubb.android.client.activities.budgetbook.BudgetBookModel;
+import de.g18.ubb.common.domain.BudgetBook;
+import de.g18.ubb.common.service.repository.ServiceRepository;
 
 public class CategorieCreateActivity extends Activity {
 
-    @Override
+	private ArrayList<BudgetBookModel> transferredData;
+	
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categorie_create);
+        
+        Button b = new Button(getApplicationContext());
+        b.setOnClickListener(new CreateListener());
     }
 
     @Override
@@ -30,6 +44,24 @@ public class CategorieCreateActivity extends Activity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+    
+    public class CreateListener implements OnClickListener {
+
+		public void onClick(View v) {
+			EditText et = (EditText) findViewById(R.id.e_categorie_create);
+			String categorie = et.getText().toString();
+			// speichere Kategorie
+			
+			
+			Bundle b = getIntent().getExtras();
+			transferredData = b.getParcelableArrayList("SingleBudgetBook");
+			Long i  = transferredData.get(0).getId();
+			BudgetBook bb = ServiceRepository.getBudgetBookService().loadSinglebudgetBookById(i);
+			
+			
+		}
+    	
     }
 
 }
--- UBBAndroidClient/src/de/g18/ubb/android/client/activities/category/CategoryOverviewActivity.java
+++ UBBAndroidClient/src/de/g18/ubb/android/client/activities/category/CategoryOverviewActivity.java
@@ -1,11 +1,8 @@
 package de.g18.ubb.android.client.activities.category;
 
 import de.g18.ubb.android.client.R;
-import de.g18.ubb.android.client.R.id;
-import de.g18.ubb.android.client.R.layout;
-import de.g18.ubb.android.client.R.menu;
-import de.g18.ubb.android.client.activities.AbstractActivity;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -31,11 +28,14 @@ public class CategoryOverviewActivity extends Activity {
 		case R.id.b_category_create:
 			Toast.makeText(this, "Kategorie neuerstellen ausgewählt",
 					Toast.LENGTH_SHORT).show();
-			
+			Intent i = new Intent(getApplicationContext(), CategorieCreateActivity.class);
+			startActivity(i);
 			break;
 		case R.id.category_change:
 			Toast.makeText(this, "Kategorie löschen ausgewählt",
 					Toast.LENGTH_SHORT).show();
+			Intent x = new Intent(getApplicationContext(), CategoryChangeActivity.class);
+			startActivity(x);
 			break;
 
 		default:
@@ -44,7 +44,5 @@ public class CategoryOverviewActivity extends Activity {
 
 		return true;
 	}
-	
-	
 
 }
