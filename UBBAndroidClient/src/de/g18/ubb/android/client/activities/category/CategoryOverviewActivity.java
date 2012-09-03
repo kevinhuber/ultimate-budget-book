cba4adaaaf669ba08ac027970b0388a129924ba6
Kevin Huber <kevinhuber.kh@gmail.com>
2012-09-03 12:43:28 +0200

    Umstellung auf AbstractActivity

--- UBBAndroidClient/src/de/g18/ubb/android/client/activities/category/CategoryOverviewActivity.java
+++ UBBAndroidClient/src/de/g18/ubb/android/client/activities/category/CategoryOverviewActivity.java
@@ -2,30 +2,49 @@ package de.g18.ubb.android.client.activities.category;
 
 import java.util.ArrayList;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import de.g18.ubb.android.client.R;
+import de.g18.ubb.android.client.activities.AbstractActivity;
 import de.g18.ubb.android.client.activities.budgetbook.BudgetBookModel;
 
-public class CategoryOverviewActivity extends Activity {
+public class CategoryOverviewActivity extends AbstractActivity<CategoryOverviewModel> {
+
+    private Button createNewCategoryButton;
 
 	private ArrayList<BudgetBookModel> transferredData;
 
 
+    @Override
+    protected CategoryOverviewModel createModel() {
+        return new CategoryOverviewModel();
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_category_overview;
+    }
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_category_overview);
-
-		Button b = (Button) findViewById(R.CategoryOverviewLayout.createButton);
-		b.setOnClickListener(new CreateButtonListener());
 
 		Bundle bundle = getIntent().getExtras();
 		transferredData = bundle.getParcelableArrayList("SingleBudgetBook");
+
+		initComponents();
+		initEventHandling();
+	}
+
+	private void initComponents() {
+        createNewCategoryButton = (Button) findViewById(R.CategoryOverviewLayout.createButton);
+	}
+
+	private void initEventHandling() {
+        createNewCategoryButton.setOnClickListener(new CreateButtonListener());
 	}
 
 
---
+++ UBBAndroidClient/src/de/g18/ubb/android/client/activities/category/CategoryOverviewModel.java
@@ -0,0 +1,15 @@
+package de.g18.ubb.android.client.activities.category;
+
+import de.g18.ubb.common.domain.AbstractModel;
+
+/**
+ * @author <a href="mailto:kevinhuber.kh@gmail.com">Kevin Huber</a>
+ */
+public class CategoryOverviewModel extends AbstractModel {
+
+    private static final long serialVersionUID = 1L;
+
+
+    public CategoryOverviewModel() {
+    }
+}
