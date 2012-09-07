aa8b12306d168f8d54eaf797433c63306d487d68
Kevin Huber <kevinhuber.kh@gmail.com>
2012-09-07 11:22:50 +0200

    Android Client kommentiert

--- UBBAndroidClient/src/de/g18/ubb/android/client/action/AbstractWaitAction.java
+++ UBBAndroidClient/src/de/g18/ubb/android/client/action/AbstractWaitAction.java
@@ -5,6 +5,8 @@ import android.view.View;
 import android.view.View.OnClickListener;
 
 /**
+ * Erweitert die {@link AbstractWaitTask}, sodass sie an einen Button oder ähnliches angebunden werden kann.
+ *
  * @author <a href="mailto:kevinhuber.kh@gmail.com">Kevin Huber</a>
  */
 public abstract class AbstractWaitAction extends AbstractWaitTask implements OnClickListener {
--- UBBAndroidClient/src/de/g18/ubb/android/client/activities/AbstractActivity.java
+++ UBBAndroidClient/src/de/g18/ubb/android/client/activities/AbstractActivity.java
@@ -9,6 +9,9 @@ import de.g18.ubb.android.client.preferences.Preferences;
 import de.g18.ubb.common.domain.AbstractModel;
 
 /**
+ * Abstrakte Oberklasse für alle Activities, die ein {@link AbstractModel} an die {@link Activity} bindet und einige
+ * "helfer"-Metohden für Ableitende Klassen zu verfügung stellt.
+ *
  * @author <a href="mailto:kevinhuber.kh@gmail.com">Kevin Huber</a>
  */
 public abstract class AbstractActivity<_Model extends AbstractModel> extends Activity {
@@ -23,6 +26,9 @@ public abstract class AbstractActivity<_Model extends AbstractModel> extends Act
         setContentView(getLayoutId());
     }
 
+    /**
+     * Erstellt das Model der Activity "Lazy" durch den aufruf von {@link #createModel()} und gibt es zurück.
+     */
     public final _Model getModel() {
         if (model == null) {
             model = createModel();
@@ -30,6 +36,9 @@ public abstract class AbstractActivity<_Model extends AbstractModel> extends Act
         return model;
     }
 
+    /**
+     * Gibt eine "Lazy" erstellte Instanz der {@link Preferences} zurück.
+     */
     protected final Preferences getPreferences() {
         if (preferences == null) {
             preferences = new Preferences(this);
@@ -37,29 +46,45 @@ public abstract class AbstractActivity<_Model extends AbstractModel> extends Act
         return preferences;
     }
 
+    /**
+     * Öffnet die übergebene {@link Activity}.
+     */
     protected final void switchActivity(Class<? extends Activity> aActivityClass) {
         Intent myIntent = new Intent(getApplicationContext(), aActivityClass);
         startActivityForResult(myIntent, 0);
     }
 
-    protected final View bind(String aPropertyname, int aComponentId) {
-        return bind(getModel(), aPropertyname, aComponentId);
+    /**
+     * Bindet eine Property des Models anhand ihres Namens an eine {@link View} anhand ihrer id
+     * und gibt die gebundene {@link View} zurück.
+     */
+    protected final View bind(String aPropertyname, int aViewId) {
+        return bind(getModel(), aPropertyname, aViewId);
     }
 
+    /**
+     * Bindet eine Property des Models anhand ihres Namens an eine {@link View} anhand ihrer id
+     * und gibt die gebundene {@link View} zurück.
+     */
     protected final View bind(AbstractModel aModel, String aPropertyname, int aComponentId) {
         return bind(aModel, aPropertyname, aComponentId, View.class);
     }
 
-    protected final <_ComponentType extends View> _ComponentType bind(String aPropertyname,
-                                                                      int aComponentId,
+    /**
+     * Bindet eine Property des Models anhand ihres Namens an eine {@link View} anhand ihrer id
+     * und gibt die gebundene und gecastete {@link View} zurück.
+     */
+    protected final <_ComponentType extends View> _ComponentType bind(String aPropertyname, int aComponentId,
                                                                       Class<? extends _ComponentType> aComponentType) {
         return bind(getModel(), aPropertyname, aComponentId, aComponentType);
     }
 
+    /**
+     * Bindet eine Property des Models anhand ihres Namens an eine {@link View} anhand ihrer id
+     * und gibt die gebundene und gecastete {@link View} zurück.
+     */
     @SuppressWarnings("unchecked")
-    protected final <_ComponentType extends View> _ComponentType bind(AbstractModel aModel,
-                                                                      String aPropertyname,
-                                                                      int aComponentId,
+    protected final <_ComponentType extends View> _ComponentType bind(AbstractModel aModel, String aPropertyname, int aComponentId,
                                                                       Class<? extends _ComponentType> aComponentType) {
         _ComponentType component = (_ComponentType) findViewById(aComponentId);
         BindingUtils.bind(component, getModel(), aPropertyname);
@@ -70,7 +95,13 @@ public abstract class AbstractActivity<_Model extends AbstractModel> extends Act
     // Abstract behavior
     // -------------------------------------------------------------------------
 
+    /**
+     * Erstellt das {@link AbstractModel}, dass in mit dieser {@link Activity} gebunden werden soll.
+     */
     protected abstract _Model createModel();
 
+    /**
+     * Gibt die Id des Layouts der {@link Activity} zurück.
+     */
     protected abstract int getLayoutId();
 }
