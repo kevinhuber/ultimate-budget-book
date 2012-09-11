df5c2777b51ba0e4f7178d14e2437c212b735d09
Sebastian.Kopatz <skopatz@gmx.net>
2012-09-12 00:49:45 +0200

    Grundvalidierung für BudgetBookerstellenFormular implementiert

--- UBBAndroidClient/src/de/g18/ubb/android/client/activities/budgetbook/BudgetBookCreateNewValidator.java
+++ UBBAndroidClient/src/de/g18/ubb/android/client/activities/budgetbook/BudgetBookCreateNewValidator.java
@@ -1,11 +1,10 @@
 package de.g18.ubb.android.client.activities.budgetbook;
 
+import de.g18.ubb.android.client.activities.booking.BookingResource;
 import de.g18.ubb.android.client.validation.AbstractValidator;
+import de.g18.ubb.android.client.validation.ValidationUtil;
 import de.g18.ubb.common.util.StringUtil;
 
-/**
- * @author <a href="mailto:kevinhuber.kh@gmail.com">Kevin Huber</a>
- */
 public class BudgetBookCreateNewValidator extends AbstractValidator<BudgetBookCreateNewModel> {
 
     public BudgetBookCreateNewValidator(BudgetBookCreateNewModel aModel) {
@@ -14,6 +13,10 @@ public class BudgetBookCreateNewValidator extends AbstractValidator<BudgetBookCr
 
     @Override
     protected String computeValidationResult() {
-        return StringUtil.EMPTY;
+    	if (StringUtil.isEmpty(getModel().getName())) {
+			return ValidationUtil
+					.createMustNotBeEmptyMessage(BudgetBookResource.PROPERTY_NAME);
+		}
+    	return ValidationUtil.createEmptyMessage();
     }
 }
---
+++ UBBAndroidClient/src/de/g18/ubb/android/client/activities/budgetbook/BudgetBookResource.java
@@ -0,0 +1,26 @@
+package de.g18.ubb.android.client.activities.budgetbook;
+
+import java.text.MessageFormat;
+
+import de.g18.ubb.common.resource.Resource;
+
+public enum BudgetBookResource implements Resource {
+
+    PROPERTY_NAME("name"),
+    PROPERTY_CATEGORIES("categories"),
+    PROPERTY_BOOKINGS("bookings"),
+    PROPERTY_ASSIGNED_USER("assignedUser"),
+    
+    MESSAGE_ADD_BUDGET_BOOK_USER("Der angegebene Benutzer ist diesem Haushaltsbuch bereits zugewiesen!"),
+    ;
+
+    private final String unformattedValue;
+
+    private BudgetBookResource(String aUnformattedValue) {
+        unformattedValue = aUnformattedValue;
+    }
+
+    public String formatted(Object... aParameters) {
+        return MessageFormat.format(unformattedValue, aParameters);
+    }
+}
