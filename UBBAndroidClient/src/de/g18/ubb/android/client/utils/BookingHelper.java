03d0a09b399a4c2ddafd1d57473ff8fe0ada2b82
sk9 <skopatz@gmx.net>
2012-09-14 10:37:51 +0300

    Update UBBAndroidClient/src/de/g18/ubb/android/client/utils/BookingHelper.java
    Dokumentierung der BookingHelper Klasse

--- UBBAndroidClient/src/de/g18/ubb/android/client/utils/BookingHelper.java
+++ UBBAndroidClient/src/de/g18/ubb/android/client/utils/BookingHelper.java
@@ -9,31 +9,76 @@ import de.g18.ubb.common.domain.Booking;
 import de.g18.ubb.common.domain.BudgetBook;
 import de.g18.ubb.common.domain.enumType.BookingType;
 
+/**
+ * Helferklasse zur Aufbereitung der Buchungen.
+ * Hier werden die Listen mit allen Buchungen
+ * entsprechend der benötigten Ansicht aufbereitet und zurück gegeben
+ *
+ * 
+ * @author Sebastian Kopatz
+ */
 public final class BookingHelper {
 
+    
     private BookingHelper() {
         // prevent instantiation
     }
-
+    
+    /**
+     * Bereitet die Buchungsliste so auf,
+     * das nur noch Buchungen des aktuellen tages enthalten sind.
+     * 
+     * @param aBookingList
+     * @return Gibt eine aufbereitet Liste vom Typ {@link Booking} zurück,
+     * die nur noch alle Buchungen des aktuellen Tages beinhaltet.
+     */
     public static List<Booking> getBookingsForCurrentDay(List<Booking> aBookingList) {
         Date currentDate = new Date();
         return getBookingsBetween(aBookingList,
                                        DateUtil.getMinDate(currentDate),
                                        DateUtil.getMaxDate(currentDate));
     }
-
+    
+    /**
+     * Bereitet die Buchungsliste so auf,
+     * das nur noch Buchungen der aktuellen Woche enthalten sind.
+     * 
+     * @param aBookingList
+     * @return Gibt eine aufbereitet Liste vom Typ {@link Booking} zurück,
+     * die nur noch alle Buchungen der aktuellen Woche beinhaltet.
+     */
     public static List<Booking> getBookingsForCurrentWeek(List<Booking> aBookingList) {
         return getBookingsListForCurrentWeek(aBookingList);
     }
-
+    
+    /**
+     * Bereitet die Buchungsliste so auf,
+     * das nur noch Buchungen des aktuellen Monats enthalten sind.
+     * 
+     * @param aBookingList
+     * @return Gibt eine aufbereitet Liste vom Typ {@link Booking} zurück,
+     * die nur noch alle Buchungen des aktuellen Monats beinhaltet.
+     */
     public static List<Booking> getBookingsForCurrentMonth(List<Booking> aBookingList) {
         return getBookingsListForCurrentMonth(aBookingList);
     }
-
+    
+    /**
+     * Bereitet die Buchungsliste so auf,
+     * das nur noch Buchungen des aktuellen Jahres enthalten sind.
+     * 
+     * @param aBookingList
+     * @return Gibt eine aufbereitet Liste vom Typ {@link Booking} zurück,
+     * die nur noch alle Buchungen des aktuellen Jahres beinhaltet.
+     */
     public static List<Booking> getBookingsForCurrentYear(List<Booking> aBookingList) {
         return getBookingsWithGivenYear(aBookingList);
     }
-
+    
+    /**
+     * @param aBookingList
+     * @return Gibt den Betrag aller Buchungen für den aktuellen Tag als float zurück.
+     */
     public static float calculateAmmountForCurrentDay(BudgetBook aBudgetBook) {
         return calculateAmmountForCurrentDay(aBudgetBook.getBookings());
     }
@@ -45,6 +90,12 @@ public final class BookingHelper {
                                        DateUtil.getMaxDate(currentDate));
     }
 
+    /**
+     * Berechnet den Betrag aller Buchungen die zwischen dem Min- und Max-Datum liegen
+     * 
+     * @param aBookingList, aMinDate, aMaxDate
+     * @return Gibt den Betrag alles Buchungen innerhalb des definierten Zeitraumes zurück
+     */
     public static float calculateAmmountBetween(List<Booking> aBookingList, Date aMinDate, Date aMaxDate) {
         float currentAmmount = 0.0f;
         for (Booking booking : aBookingList) {
@@ -58,7 +109,7 @@ public final class BookingHelper {
         }
         return currentAmmount;
     }
-
+    
     public static List<Booking> getBookingsBetween(List<Booking> aBookingList, Date aMinDate, Date aMaxDate) {
     	List<Booking> tmpBookingList = new ArrayList<Booking>();
         for (Booking booking : aBookingList) {
@@ -68,16 +119,16 @@ public final class BookingHelper {
         }
         return tmpBookingList;
     }
-
+    
     public static List<Booking> getBookingsWithGivenYear(List<Booking> aBookingList) {
     	List<Booking> tmpBookingList = new ArrayList<Booking>();
-
+    	
     	Calendar beginningYearDate = Calendar.getInstance();
     	beginningYearDate.set(Calendar.DAY_OF_YEAR, 1);
     	beginningYearDate.set(Calendar.MONTH, 1);
-
+    	
     	Date currentDate = new Date();
-
+    	
         for (Booking booking : aBookingList) {
             if (DateUtil.isBetween(booking.getBookingTime(), beginningYearDate.getTime(), currentDate)) {
             	tmpBookingList.add(booking);
@@ -85,15 +136,15 @@ public final class BookingHelper {
         }
         return tmpBookingList;
     }
-
+    
     public static List<Booking> getBookingsListForCurrentWeek(List<Booking> aBookingList) {
     	List<Booking> tmpBookingList = new ArrayList<Booking>();
-
+    	
     	Calendar beginningWeekDate = Calendar.getInstance();
     	beginningWeekDate.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
-
+    	
     	Date currentDate = new Date();
-
+    	
         for (Booking booking : aBookingList) {
             if (DateUtil.isBetween(booking.getBookingTime(), beginningWeekDate.getTime(), currentDate)) {
             	tmpBookingList.add(booking);
@@ -101,15 +152,15 @@ public final class BookingHelper {
         }
         return tmpBookingList;
     }
-
+    
     public static List<Booking> getBookingsListForCurrentMonth(List<Booking> aBookingList) {
     	List<Booking> tmpBookingList = new ArrayList<Booking>();
-
+    	
     	Calendar beginningYearDate = Calendar.getInstance();
     	beginningYearDate.set(Calendar.DAY_OF_MONTH, 1);
-
+    	
     	Date currentDate = new Date();
-
+    	
         for (Booking booking : aBookingList) {
             if (DateUtil.isBetween(booking.getBookingTime(), beginningYearDate.getTime(), currentDate)) {
             	tmpBookingList.add(booking);
